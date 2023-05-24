9639a00f20d6b7701396f0ea70dd666da42b2933
krigovszkibalint <krigovszki.balint@gmail.com>
2023-05-24 05:51:55 +0200

    Variables default values

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "answer_1" {
   description = "Answer 1"
   type        = string
+  default     = " "
 }
 
 variable "answer_2" {
   description = "Answer 2"
   type        = string
+  default     = " "
 }
 
 variable "answer_3" {
   description = "Answer 3"
   type        = string
+  default     = " "
 }
 
 variable "answer_4" {
   description = "Answer 4"
   type        = string
+  default     = " "
 }
 
 variable "answer_5" {
   description = "Answer 5"
   type        = string
+  default     = " "
 }
