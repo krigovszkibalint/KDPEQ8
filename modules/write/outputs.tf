58b85ddca33bb0c9f3a280beaaed806466d8fc53
krigovszkibalint <krigovszki.balint@gmail.com>
2023-05-24 05:30:59 +0200

    Task 3 fix

--- modules/write/outputs.tf
+++ modules/write/outputs.tf
@@ -1,23 +1,19 @@
-output "file_content_md5" {
-  value = local_file.file[0].content_md5
+output "task3_answer_1" {
+  value = var.answer_1
 }
 
-output "answer_1" {
-  value = local_file.file.answer_1
+output "task3_answer_2" {
+  value = var.answer_2
 }
 
-output "answer_2" {
-  value = local_file.file.answer_2
+output "task3_answer_3" {
+  value = var.answer_3
 }
 
-output "answer_3" {
-  value = local_file.file.answer_3
+output "task3_answer_4" {
+  value = var.answer_4
 }
 
-output "answer_4" {
-  value = local_file.file.answer_4
-}
-
-output "answer_5" {
-  value = local_file.file.answer_5
+output "task3_answer_5" {
+  value = var.answer_5
 }
